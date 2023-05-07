c7929570e0f61d89bf648d67513aaf92722c81cd
takathius2 <106050338+takathius2@users.noreply.github.com>
2023-05-07 17:11:57 +0900

    Gitへプッシュする

---
+++ sevendays/src/day1/Sample101.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package day1;
+
+/**
+ * @author takathius2
+ *
+ */
+public class Sample101 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		hoge
+		System.out.println(123);
+		System.out.println(456);
+	}
+
+}
