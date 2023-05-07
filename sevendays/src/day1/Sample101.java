9c6fbc3b3bfc381def5ddf1fda5b746188d57740
takathius2 <106050338+takathius2@users.noreply.github.com>
2023-05-07 17:37:06 +0900

    誤字修正

--- sevendays/src/day1/Sample101.java
+++ sevendays/src/day1/Sample101.java
@@ -26,20 +26,28 @@ public class Sample101 {
 		 * 	プログラムを実行するときの“引数の文言”を受け取る
 		 */
 		// TODO 自動生成されたメソッド・スタブ
-//		HolloWorld　という文字列を表示する
-		System.out.println("Hollo,World");
+			
+		//HolloWorld　という文字列を表示する
 		
-//		printとprintlnの違い
+		System.out.println("Hollo,World");
+		// 文字列はダブルクォーテーションで囲む必要がある
+		
+		//printとprintlnの違い
+		/*
+		 * printでは処理の実行結果を改行せず出力する
+		 * printlnでは処理の実行結果を改行して出力する
+		 */
 		
 		System.out.print(123);
 		System.out.println(546);
 		System.out.println(789);
 		System.out.println("Hollo,World");
+		
 		/*
-		 * printでは処理の実行結果を改行せず出力する
-		 * printlnでは処理の実行結果を改行して出力する
-		 * また、文字列は
+		 * エラーが起きるとRubyと同じくメソッドや文法・ミスしてる箇所
+		 * を指摘してくれるのでそこを見る
 		 */
+		
 	}
 
 }
