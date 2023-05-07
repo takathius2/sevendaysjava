28c3db00bddc8adeb319fbe5ffb4b98c9bc71da4
takathius2 <106050338+takathius2@users.noreply.github.com>
2023-05-07 17:32:06 +0900

    1日目終了。 Javaの基礎分歩を学び、GitnoリモートリポジトリへPusuも行えた

--- sevendays/src/day1/Sample101.java
+++ sevendays/src/day1/Sample101.java
@@ -2,21 +2,44 @@
  * 
  */
 package day1;
-
+//以下はday1というパッケージに属しています
 /**
  * @author takathius2
  *
  */
 public class Sample101 {
-
+//同一パッケージ内に同じクラスは存在しないようにする
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/*
+		 * Public=どのクラスからもアクセスできる。公開範囲が一番広い
+		 * 	↑を変えて公開範囲を限定する
+		 * 
+		 * Static=固定という意味。クラス特有の値やメソッド
+		 * 	インスタンス化（new）せず使えるようになる
+		 * 
+		 * Void=main関数内似記述したものが実行される
+		 * 
+		 * String args=コマンドライン引数。
+		 * 	プログラムを実行するときの“引数の文言”を受け取る
+		 */
 		// TODO 自動生成されたメソッド・スタブ
-//		hoge
-		System.out.println(123);
-		System.out.println(456);
+//		HolloWorld　という文字列を表示する
+		System.out.println("Hollo,World");
+		
+//		printとprintlnの違い
+		
+		System.out.print(123);
+		System.out.println(546);
+		System.out.println(789);
+		System.out.println("Hollo,World");
+		/*
+		 * printでは処理の実行結果を改行せず出力する
+		 * printlnでは処理の実行結果を改行して出力する
+		 * また、文字列は
+		 */
 	}
 
 }
